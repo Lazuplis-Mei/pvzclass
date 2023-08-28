3ca78ccdb9fd1c6d9a9966bd40bc68a84d9c1ca8
Violet-Nonbloosom <silkqcek@yeah.net>
2023-08-28 20:55:33 +0800

    合并 README，补充 AsmFunction
    补充若干汇编宏。
    合并两分差距极大的 README，并且根据更新进行补充。

--- pvzclass/Enums/DamageType.h
+++ pvzclass/Enums/DamageType.h
@@ -10,6 +10,8 @@ namespace DamageType
 		Direct_Ice = 4,
 		Side_Ice,
 		Sputter_Ice,
+		Doesnt_Cause_Flash = 8,
+		Doesnt_Leave_Body = 16,
 		Direct_AntiVehicle = 32,
 		Side_AntiVehicle,
 		Sputter_AntiVehicle,
