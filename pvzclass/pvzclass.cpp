058fa0c3655dd9ff0fa72be17dd933f1b9c22746
Violet-Nonbloosom <86547296+Violet-Nonbloosom@users.noreply.github.com>
2023-11-23 12:43:20 +0800

    Add VaseOpenEvent

--- pvzclass/pvzclass.cpp
+++ pvzclass/pvzclass.cpp
@@ -1,9 +1,37 @@
 ﻿#include "pvzclass.h"
+#include "Events/EventHandler.h"
+#include "Events/VaseOpenEvent.h"
 #include <iostream>
 #include <thread>
 
 using std::cout;
 using std::endl;
+
+SPT<PVZ::Vase> listenerC(SPT<PVZ::Vase> vase, bool& cancelled)
+{
+	if (vase->Skin == VaseSkin::VaseSkinLeaf)
+	{
+		vase->Skin = VaseSkin::VaseSkinZombie;
+		cancelled = true;
+	}
+	return(vase);
+}
+
+SPT<PVZ::Vase> listenerM(SPT<PVZ::Vase> vase, bool& cancelled)
+{
+	std::vector<SPT<PVZ::Griditem>> vases = PVZ::GetBoard()->GetAllGriditems();
+	while (vases[vases.size() - 1]->Type != GriditemType::Vase)
+		vases.pop_back();
+	SPT<PVZ::Griditem> cur = vases[vases.size() - 1];
+	SPT<PVZ::Vase> curVase = MKS<PVZ::Vase>(cur->GetBaseAddress());
+	if (vase->Skin == VaseSkin::VaseSkinZombie)
+	{
+		curVase->Column = 0;
+		return(curVase);
+	}
+	else
+		return(vase);
+}
 
 int main()
 {
@@ -11,25 +39,22 @@ int main()
 	if (!pid) return 1;
 	PVZ::InitPVZ(pid);
 
+	VaseOpenEvent e;
+	e.addListener(listenerC);
+	e.addListener(listenerM);
+	EventHandler handler;
+	handler.addEvent(MKS<VaseOpenEvent>(e));
+	handler.start();
+
 	EnableBackgroundRunning();
 	auto app = PVZ::GetPVZApp();
-	while (app->GameState != PVZGameState::Playing) Sleep(10);
 
 	while (true)
 	{
-		PVZ::Memory::immediateExecute = true;
-		PVZ::Memory::WaitPVZ();
-		for (int row = 0; row < 5; row++)
-		{
-			for (int col = 1; col < 9; col++)
-			{
-				Creator::CreateZombie(ZombieType::Zombie, row, col);
-			}
-		}
-		PVZ::Memory::ResumePVZ();
-		system("pause");
+		handler.run(1);
 	}
 
+	handler.stop();
 	PVZ::QuitPVZ();
 	return 0;
 }
